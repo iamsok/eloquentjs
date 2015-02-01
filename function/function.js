3da65f10395e44cfcbacbe071e31d711a18b46e0
David Sok <sokd88@gmail.com>
2015-01-31 20:21:11 -0500

    more function practice

--- function/function.js
+++ function/function.js
@@ -1,9 +1,11 @@
+//square a number
 var square = function(x) {
   return x * x;
 };
 
 console.log(square(12));
 
+//power of a number n^y-power
 var power = function(base, exponent){
   var result = 1;
   for (var count = 0; count < exponent; count++)
@@ -12,3 +14,26 @@ var power = function(base, exponent){
 };
 
 console.log(power(2,10));
+
+var landscape = function(){
+  var result = "";
+  var flat = function(size) {
+    for (var count = 0; count < size; count++)
+      result += "_";
+  };
+  var mountain = function(size) {
+    result += "/";
+    for (var count = 0; count < size; count ++)
+      result += "'";
+    result += "\\";
+  };
+
+  flat(3);
+  mountain(4);
+  flat(6);
+  mountain(1);
+  flat(1);
+  return result;
+};
+
+console.log(landscape());
